2274ba18aa64519bf798a10f7f54dc649d66e732
Mich Iline <mislav@pm.me>
2023-07-27 00:02:32 +0200

    updaated cover image component to run only on client

--- app/_components/Cover.tsx
+++ app/_components/Cover.tsx
@@ -1,6 +1,9 @@
 'use client'
 import Image from 'next/image'
 import Overlay from './Overlay'
+import dynamic from 'next/dynamic'
+
+const DynamicCoverImage = dynamic(() => import('./CoverImage'), { ssr: false })
 
 interface CoverProps {
 	id: string
@@ -18,23 +21,9 @@ interface CoverProps {
 }
 
 const Cover = ({ id, img, handleClick }: CoverProps) => {
-	const currImg = window.innerWidth <= 768 ? img.mobile : img.web
 	return (
 		<div className='w-full h-screen relative flex justify-center items-center'>
-			<Image
-				src={`/${currImg.src}`}
-				blurDataURL={currImg.blurData}
-				alt='Cover image'
-				layout='fill'
-				placeholder='blur'
-				quality={100}
-				sizes={'100vw'}
-				priority={true}
-				style={{
-					objectFit: 'cover',
-					objectPosition: 'center',
-				}}
-			/>
+			<DynamicCoverImage img={img} />
 			<Overlay />
 			<div className='flex flex-col justify-center items-center z-10 text-white'>
 				<h1 className='h-min text-6xl text-center'>{id}</h1>
---
+++ app/_components/CoverImage.tsx
@@ -0,0 +1,36 @@
+import Image from 'next/image'
+
+interface CoverImageProps {
+	img: {
+		mobile: {
+			src: string
+			blurData: string
+		}
+		web: {
+			src: string
+			blurData: string
+		}
+	}
+}
+
+const CoverImage = ({ img }: CoverImageProps) => {
+	const currImg = window && window.innerWidth <= 768 ? img.mobile : img.web
+	return (
+		<Image
+			src={`/${currImg.src}`}
+			blurDataURL={currImg.blurData}
+			alt='Cover image'
+			layout='fill'
+			placeholder='blur'
+			quality={100}
+			sizes={'100vw'}
+			priority={true}
+			style={{
+				objectFit: 'cover',
+				objectPosition: 'center',
+			}}
+		/>
+	)
+}
+
+export default CoverImage
