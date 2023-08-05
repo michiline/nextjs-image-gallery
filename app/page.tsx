03b7f2152c1e33bc6e7cab5c2e375f1ddb8cfa2f
Mich Iline <mislav@pm.me>
2023-08-05 17:56:27 +0200

    gallery page index fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,14 @@ import Link from 'next/link'
 
 export default function Home() {
 	return (
-		<div className='flex w-full px-6 py-6'>
+		<div className='flex flex-col w-full px-6 py-6'>
 			<h1 className='text-lg font-bold'>NextJS Image Gallery</h1>
 			<p className='pt-4'>
 				Image gallery which consists of justified grid and full screen
-				gallery. Built with NextJS and TailwindCSS.
+				gallery.
 			</p>
-			<Link href='/gallery/nina'>
+			<p className='pt-2'>Built with NextJS and TailwindCSS.</p>
+			<Link href='/gallery/natalija-emanuel'>
 				<button className='border border-black h-[45px] px-8 mt-8 text-md hover:bg-black hover:text-white transition ease-in-out duration-150'>
 					View Gallery
 				</button>
