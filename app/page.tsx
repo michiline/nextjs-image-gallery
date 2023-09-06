ed7c2cbb2698d73410d47293bdb9b6759f608380
Mich Iline <mislav@pm.me>
2023-09-06 15:14:22 +0200

    added og tags

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,28 @@
 import Link from 'next/link'
 
+import type { Metadata } from 'next'
+
+export async function generateMetadata(): Promise<Metadata> {
+	return {
+		metadataBase: new URL('https://gallery.mivandic.com'),
+		title: 'NextJS Image Gallery',
+		description: `NextJS justified grid and fullscreen gallery.`,
+		openGraph: {
+			type: 'website',
+			siteName: 'NextJS Image Gallery',
+			description: `NextJS justified grid and fullscreen gallery.`,
+			title: 'NextJS Image Gallery',
+			url: `https://gallery.mivandic.com`,
+		},
+	}
+}
+
 export default function Home() {
 	return (
 		<div className='flex flex-col w-full px-6 py-6'>
 			<h1 className='text-lg font-bold'>NextJS Image Gallery</h1>
 			<p className='pt-4'>
-				Image gallery consisting of justified grid and full screen
+				Image gallery consisting of justified grid and fullscreen
 				gallery.
 			</p>
 			<p className='pt-2'>Built with NextJS and TailwindCSS.</p>
